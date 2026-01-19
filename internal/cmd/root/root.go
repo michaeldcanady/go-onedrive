6284198f1d0ff489aa00a5a9ff37205f6b8e1f02
Michael Canady <michaeldcanady@gmail.com>
2026-01-19 00:35:49 +0000

    cache path parent not initialized

--- internal/cmd/root/root.go
+++ internal/cmd/root/root.go
@@ -3,10 +3,12 @@ package root
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/michaeldcanady/go-onedrive/internal/cmd/auth"
 	"github.com/michaeldcanady/go-onedrive/internal/cmd/ls"
 	"github.com/michaeldcanady/go-onedrive/internal/di"
+	"github.com/michaeldcanady/go-onedrive/internal/logging"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -19,6 +21,17 @@ const (
 
 // CreateRootCmd constructs the root command for the CLI application.
 func CreateRootCmd() (*cobra.Command, error) {
+	ctx := context.Background()
+	container, err := di.NewContainer(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize container: %w", err)
+	}
+
+	cliLogger, err := container.LoggerService.CreateLogger("cli")
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize cli logger: %w", err)
+	}
+
 	rootCmd := &cobra.Command{
 		Use:   "odc",
 		Short: "A OneDrive CLI client",
@@ -49,7 +62,11 @@ Examples:
 	rootCmd.PersistentFlags().String("config", defaultConfigFile, "path to config file")
 	configPath, err := rootCmd.PersistentFlags().GetString("config")
 	if err != nil {
+		cliLogger.Error("failed to get user provided config file", logging.String("error", err.Error()))
 		return nil, fmt.Errorf("failed to get config flag: %w", err)
+	}
+	if strings.TrimSpace(configPath) != "" {
+		cliLogger.Debug("user provided config", logging.String("path", configPath))
 	}
 
 	viper.SetConfigFile(configPath)
@@ -67,17 +84,6 @@ Examples:
 	rootCmd.PersistentFlags().Bool(loggingLevelFlagLong, false, loggingLevelUsage)
 	if err := viper.BindPFlag("logging.level", rootCmd.PersistentFlags().Lookup(loggingLevelFlagLong)); err != nil {
 		return nil, fmt.Errorf("failed to bind logging level flag: %w", err)
-	}
-
-	ctx := context.Background()
-	container, err := di.NewContainer(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed to initialize container: %w", err)
-	}
-
-	cliLogger, err := container.LoggerService.CreateLogger("cli")
-	if err != nil {
-		return nil, err
 	}
 
 	rootCmd.AddCommand(
