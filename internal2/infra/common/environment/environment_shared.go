6a96d9e57f33d809708d6feb95995d3c1a50f263
Michael Canady <michaeldcanady@gmail.com>
2026-02-12 03:22:58 +0000

    add environment variable support
    allows for easier testing and container implementation

---
+++ internal2/infra/common/environment/environment_shared.go
@@ -0,0 +1,12 @@
+package environment
+
+const (
+	EnvLogLevel  = "ODC_LOG_LEVEL"
+	EnvLogOutput = "ODC_LOG_OUTPUT"
+	EnvLogDir    = "ODC_LOG_PATH"
+	EnvTempDir   = "ODC_TEMP_DIR"
+	EnvCacheDir  = "ODC_CACHE_DIR"
+	EnvDataDir   = "ODC_DATA_DIR"
+	EnvConfigDir = "ODC_CONFIG_DIR"
+	EnvStateDir  = "ODC_STATE_DIR"
+)
