3c03f40c7eccbe39ccc2f6ec0e54399a637029e3
Michael Canady <michaeldcanady@gmail.com>
2026-02-14 03:44:45 +0000

    serializing auth type

--- internal2/infra/auth/auth_type.go
+++ internal2/infra/auth/auth_type.go
@@ -14,8 +14,54 @@ const (
 
 type AuthType int32
 
-func (a AuthType) String() string {
+func ParseAuthType(str string) AuthType {
+	authType, ok := map[string]AuthType{
+		AuthTypeUnknownString:            AuthTypeUnknown,
+		AuthTypeInteractiveBrowserString: AuthTypeInteractiveBrowser,
+		AuthTypeDeviceCodeString:         AuthTypeDeviceCode,
+		AuthTypeROPCString:               AuthTypeROPC,
+		AuthTypeClientSecretString:       AuthTypeClientSecret,
+	}[str]
+	if !ok {
+		return AuthTypeUnknown
+	}
+	return authType
+}
 
+func (a *AuthType) UnmarshalJSON(data []byte) error {
+	str := string(data)
+	*a = ParseAuthType(str)
+	return nil
+}
+
+func (a AuthType) MarshalJSON() ([]byte, error) {
+	str := a.String()
+	return []byte(str), nil
+}
+
+func (a *AuthType) UnmarshalYAML(data []byte) error {
+	str := string(data)
+	*a = ParseAuthType(str)
+	return nil
+}
+
+func (a AuthType) MarshalYAML() (interface{}, error) {
+	str := a.String()
+	return str, nil
+}
+
+func (a AuthType) String() string {
+	str, ok := map[AuthType]string{
+		AuthTypeUnknown:            AuthTypeUnknownString,
+		AuthTypeInteractiveBrowser: AuthTypeInteractiveBrowserString,
+		AuthTypeDeviceCode:         AuthTypeDeviceCodeString,
+		AuthTypeROPC:               AuthTypeROPCString,
+		AuthTypeClientSecret:       AuthTypeClientSecretString,
+	}[a]
+	if !ok {
+		return AuthTypeUnknown.String()
+	}
+	return str
 }
 
 const (
