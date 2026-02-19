8680238904a5a60f74757d3f05e5553f89da54f8
Michael Canady <michaeldcanady@gmail.com>
2026-02-19 16:47:07 +0000

    add json support

--- internal2/domain/file/item_type.go
+++ internal2/domain/file/item_type.go
@@ -1,5 +1,10 @@
 package file
 
+import (
+	"encoding/json"
+	"strings"
+)
+
 type ItemType int
 
 const (
@@ -7,3 +12,41 @@ const (
 	ItemTypeFile
 	ItemTypeFolder
 )
+
+func ParseItemType(str string) ItemType {
+	ype, ok := map[string]ItemType{
+		"file":    ItemTypeFile,
+		"folder":  ItemTypeFolder,
+		"unknown": ItemTypeUnknown,
+	}[strings.ToLower(str)]
+	if !ok {
+		return ItemTypeUnknown
+	}
+	return ype
+}
+
+func (i ItemType) String() string {
+	str, ok := map[ItemType]string{
+		ItemTypeFile:    "file",
+		ItemTypeFolder:  "folder",
+		ItemTypeUnknown: "unknown",
+	}[i]
+	if !ok {
+		return "unknown"
+	}
+	return str
+}
+
+func (i ItemType) MarshalJSON() ([]byte, error) {
+	return json.Marshal(i.String())
+}
+
+func (i *ItemType) UnmarshalJSON(b []byte) error {
+	var s string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+
+	*i = ParseItemType(s)
+	return nil
+}
