ff90a92235b16c09b522fbc8390913725df52ebb
michaeldcanady <michaeldcanady@gmail.com>
2026-03-10 03:29:25 +0000

    no odc

---
+++ internal2/app/fs/onedrive/cached_contents_repository.go
@@ -0,0 +1,115 @@
+package onedrive
+
+import (
+	"bytes"
+	"context"
+	"io"
+
+	"github.com/michaeldcanady/go-onedrive/internal2/domain/common/logger"
+	"github.com/michaeldcanady/go-onedrive/internal2/domain/file"
+	infrafile "github.com/michaeldcanady/go-onedrive/internal2/infra/file"
+)
+
+type CachedFileContentsRepository struct {
+	gateway       infrafile.GraphContentsGateway
+	contentCache  infrafile.ContentsCache
+	metadataCache infrafile.MetadataCache
+	pathIDCache   infrafile.PathIDCache
+	log           logger.Logger
+}
+
+func NewCachedFileContentsRepository(
+	gateway infrafile.GraphContentsGateway,
+	contentCache infrafile.ContentsCache,
+	metadataCache infrafile.MetadataCache,
+	pathIDCache infrafile.PathIDCache,
+	l logger.Logger,
+) *CachedFileContentsRepository {
+	return &CachedFileContentsRepository{
+		gateway:       gateway,
+		contentCache:  contentCache,
+		metadataCache: metadataCache,
+		pathIDCache:   pathIDCache,
+		log:           l,
+	}
+}
+
+func (r *CachedFileContentsRepository) Download(ctx context.Context, driveID, path string, opts file.DownloadOptions) (io.ReadCloser, error) {
+	log := r.log.WithContext(ctx).With(logger.String("path", path))
+
+	var cachedData []byte
+	var etag string
+
+	if !opts.NoCache {
+		cacheKey := path
+		if id, ok := r.pathIDCache.Get(ctx, path); ok {
+			cacheKey = id
+		}
+
+		if entry, ok := r.contentCache.Get(ctx, cacheKey); ok {
+			cachedData = entry.Data
+			if entry.CTag != "" {
+				etag = entry.CTag
+			}
+		}
+	}
+
+	fresh, ctag, err := r.gateway.Download(ctx, driveID, path, etag)
+	if err != nil {
+		return nil, err
+	}
+
+	if fresh == nil {
+		// 304
+		return io.NopCloser(bytes.NewReader(cachedData)), nil
+	}
+
+	if !opts.NoStore && ctag != "" {
+		cacheKey := path
+		if id, ok := r.pathIDCache.Get(ctx, path); ok {
+			cacheKey = id
+		}
+		_ = r.contentCache.Put(ctx, cacheKey, &file.Contents{
+			CTag: ctag,
+			Data: fresh,
+		})
+	}
+
+	return io.NopCloser(bytes.NewReader(fresh)), nil
+}
+
+func (r *CachedFileContentsRepository) Upload(ctx context.Context, driveID, path string, body io.Reader, opts file.UploadOptions) (*file.Metadata, error) {
+	log := r.log.WithContext(ctx).With(logger.String("path", path))
+
+	data, err := io.ReadAll(body)
+	if err != nil {
+		return nil, err
+	}
+
+	ifMatch := opts.IfMatch
+	if ifMatch == "" && !opts.Force {
+		cacheKey := path
+		if id, ok := r.pathIDCache.Get(ctx, path); ok {
+			cacheKey = id
+		}
+		if entry, ok := r.contentCache.Get(ctx, cacheKey); ok {
+			ifMatch = entry.CTag
+		}
+	}
+
+	metadata, ctag, err := r.gateway.Upload(ctx, driveID, path, data, ifMatch)
+	if err != nil {
+		return nil, err
+	}
+
+	if !opts.NoStore {
+		_ = r.contentCache.Put(ctx, metadata.ID, &file.Contents{
+			CTag: ctag,
+			Data: data,
+		})
+		_ = r.metadataCache.Put(ctx, metadata.ID, metadata)
+		_ = r.pathIDCache.Put(ctx, path, metadata.ID)
+	}
+
+	return metadata, nil
+}
