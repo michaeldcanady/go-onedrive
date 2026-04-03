cc300ab54d965e2aae39ceae6df3b382ad188bc9
michaeldcanady <michaeldcanady@gmail.com>
2026-04-03 00:11:48 +0000

    chore: add release-please

---
+++ internal/version/version.go
@@ -0,0 +1,8 @@
+// Package version provides the version number for the odc application.
+package version
+
+// Version is the current version of the odc application.
+// x-release-please-start-version
+const Version = "0.1.0"
+
+// x-release-please-end
