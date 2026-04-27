0ee2710d92c90231aca18f29105b9de210ee831b
michaeldcanady <michaeldcanady@gmail.com>
2026-04-27 01:59:29 +0000

    chore(ci): requires special orientation

--- internal/features/identity/providers/microsoft/token_utils_fuzz_test.go
+++ internal/features/identity/providers/microsoft/token_utils_fuzz_test.go
@@ -18,9 +18,8 @@ func FuzzExtractFullIdentityFromToken(f *testing.F) {
 
 func BenchmarkExtractFullIdentityFromToken(b *testing.B) {
 	// A mock token that looks like a JWT
-	// gitleaks:allow
 	// nolint:gosec // G101 // not real credentials
-	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvaWQiOiJ1c2VyMSIsInByZWZlcnJlZF91c2VybmFtZSI6InVzZXIxQGV4YW1wbGUuY29tIiwibmFtZSI6IlVzZXIgMSJ9.signature"
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvaWQiOiJ1c2VyMSIsInByZWZlcnJlZF91c2VybmFtZSI6InVzZXIxQGV4YW1wbGUuY29tIiwibmFtZSI6IlVzZXIgMSJ9.signature" // gitleaks:allow
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
